74fdc28a2551a0606be811d5367049e7bdafeb6e
aayushk720@gmail.com <aayushk720@gmail.com>
2023-01-02 23:45:05 +0530

    update

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDHaqzFIh3WpX8Hw6k55TAA8keUwyksMWU",
+    authDomain: "entertain-hub.firebaseapp.com",
+    projectId: "entertain-hub",
+    storageBucket: "entertain-hub.appspot.com",
+    messagingSenderId: "208836241783",
+    appId: "1:208836241783:web:29fe7981fb6e963f6c5972",
+    measurementId: "G-RWVCY9MR6J"
+  };
---
+++ src/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Banner from "./Banner";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+import requests from "./Requests";
+import Row from "./Row";
+
+function HomeScreen() {
+  return (
+    <div className="homeScreen">
+      <Nav />
+      <Banner />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
+      <Row title="TopRated" fetchUrl={requests.fetchTopRated} />
+      <Row title="ActionMovies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="ComedyMovies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="HorrorMovies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="RomanceMovies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default HomeScreen;
